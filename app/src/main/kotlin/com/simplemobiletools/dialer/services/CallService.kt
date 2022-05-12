8ac0a4f6e4533941effff5181505a1011eca0990
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2022-05-12 09:03:24 +0300

    add icons

--- app/src/main/kotlin/com/simplemobiletools/dialer/services/CallService.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/services/CallService.kt
@@ -1,15 +1,15 @@
 package com.simplemobiletools.dialer.services
 
-import android.app.KeyguardManager
-import android.content.Context
 import android.telecom.Call
 import android.telecom.InCallService
+import android.util.Log
 import com.simplemobiletools.dialer.activities.CallActivity
-import com.simplemobiletools.dialer.extensions.getStateCompat
 import com.simplemobiletools.dialer.extensions.isOutgoing
 import com.simplemobiletools.dialer.extensions.powerManager
 import com.simplemobiletools.dialer.helpers.CallManager
 import com.simplemobiletools.dialer.helpers.CallNotificationManager
+
+const val TAG = "SimpleDialer:CallService"
 
 class CallService : InCallService() {
     private val callNotificationManager by lazy { CallNotificationManager(this) }
@@ -17,6 +17,7 @@ class CallService : InCallService() {
     private val callListener = object : Call.Callback() {
         override fun onStateChanged(call: Call, state: Int) {
             super.onStateChanged(call, state)
+            Log.d(TAG, "onStateChanged: $call")
             if (state != Call.STATE_DISCONNECTED) {
                 callNotificationManager.setupNotification()
             }
@@ -25,23 +26,19 @@ class CallService : InCallService() {
 
     override fun onCallAdded(call: Call) {
         super.onCallAdded(call)
+        Log.d(TAG, "onCallAdded: $call")
+        if (!powerManager.isInteractive || call.isOutgoing()) {
+            startActivity(CallActivity.getStartIntent(this))
+        }
         CallManager.call = call
         CallManager.inCallService = this
         CallManager.registerCallback(callListener)
-
-        val isScreenLocked = (getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager).isDeviceLocked
-        if (!powerManager.isInteractive || call.isOutgoing() || isScreenLocked) {
-            startActivity(CallActivity.getStartIntent(this))
-            if (call.getStateCompat() != Call.STATE_RINGING) {
-                callNotificationManager.setupNotification()
-            }
-        } else {
-            callNotificationManager.setupNotification()
-        }
+        callNotificationManager.setupNotification()
     }
 
     override fun onCallRemoved(call: Call) {
         super.onCallRemoved(call)
+        Log.d(TAG, "onCallRemoved: $call")
         CallManager.call = null
         CallManager.inCallService = null
         callNotificationManager.cancelNotification()
