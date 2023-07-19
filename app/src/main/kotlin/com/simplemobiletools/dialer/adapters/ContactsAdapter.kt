8fc853e6c083d317e0c5754fa7105a62285faada
merkost <konstantinlikes@gmail.com>
2023-07-19 16:45:30 +1000

    Fixed font setting in Adapters

--- app/src/main/kotlin/com/simplemobiletools/dialer/adapters/ContactsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/adapters/ContactsAdapter.kt
@@ -49,7 +49,8 @@ class ContactsAdapter(
     ItemTouchHelperContract, MyRecyclerView.MyZoomListener {
 
     private var textToHighlight = highlightText
-    private var fontSize = activity.getTextSize()
+    private val fontSize: Float
+        get() = activity.getTextSize()
     private var touchHelper: ItemTouchHelper? = null
     private var startReorderDragListener: StartReorderDragListener? = null
     var onDragEndListener: (() -> Unit)? = null
