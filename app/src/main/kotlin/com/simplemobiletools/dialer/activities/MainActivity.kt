e7a93e7e2a25fb40579a73fe6c07448a35899913
merkost <konstantinlikes@gmail.com>
2023-07-20 11:41:39 +1000

    Added change column count animation

--- app/src/main/kotlin/com/simplemobiletools/dialer/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/activities/MainActivity.kt
@@ -231,7 +231,7 @@ class MainActivity : SimpleActivity() {
             val newColumnCount = it as Int
             if (currentColumnCount != newColumnCount) {
                 config.contactsGridColumnCount = newColumnCount
-                favorites_fragment.updateListAdapter()
+                favorites_fragment?.columnCountChanged()
             }
         }
     }
@@ -239,7 +239,7 @@ class MainActivity : SimpleActivity() {
     private fun changeViewType() {
         ChangeViewTypeDialog(this) {
             refreshMenuItems()
-            favorites_fragment.updateListAdapter()
+            favorites_fragment?.refreshItems()
         }
     }
 
